6407647fdb0bfab3fc8cc0e21ccd5775a038f32a
James Womack <womackrjames@gmail.com>
2018-02-02 18:22:07 -0800

    Added build folder for js

---
+++ build/script.clean.js
@@ -0,0 +1,7 @@
+var
+  shell = require('shelljs'),
+  path = require('path')
+
+shell.rm('-rf', path.resolve(__dirname, '../wwwroot/dist/*'))
+shell.rm('-rf', path.resolve(__dirname, '../wwwroot/dist/.*'))
+console.log(' Cleaned build artifacts.\n')
